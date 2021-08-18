059e38909796307de0ef4ba7283fca1cb1634472
linsandy29 <73650748+linsandy29@users.noreply.github.com>
2021-08-18 17:29:22 +0800

    Update ultra_mediam.py

--- ultra_mediam.py
+++ ultra_mediam.py
@@ -11,7 +11,6 @@ q2 = deque([0,0,0,0,0])
 q3 = deque([0,0,0,0,0])
 q4 = deque([0,0,0,0,0])
 q5 = deque([0,0,0,0,0])
-
  
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
@@ -50,8 +49,12 @@ def distance_one():
     StartTime = time.time()
     StopTime = time.time()
     t=0
+    tt=0
     while GPIO.input(echo_one) == 0:
         StartTime = time.time()
+        tt = StartTime - StopTime
+        if tt > 0.0058309038:      
+            break
  
     while GPIO.input(echo_one) == 1:
         StopTime = time.time()
@@ -62,13 +65,17 @@ def distance_one():
     TimeElapsed = StopTime - StartTime
     distance = (TimeElapsed * 34300) / 2
  
-    q1.popleft()
-    q1.append(distance)
-    c = q1
-    c = sorted(c)
-    size = len(c)
-    median = c[(size-1)//2]
-    return median
+    if distance>0:
+        q1.popleft()
+        q1.append(distance)
+        c = q1
+        c = sorted(c)
+        size = len(c)
+        median = c[(size-1)//2]
+        return median
+    else:
+        return 404
+
 
 def distance_two():
 
@@ -79,8 +86,12 @@ def distance_two():
     StartTime_two = time.time()
     StopTime_two = time.time()
     t=0
+    tt=0
     while GPIO.input(echo_two) == 0:
         StartTime_two = time.time()
+        tt = StartTime_two - StopTime_two
+        if tt > 0.0058309038:      
+            break
  
     while GPIO.input(echo_two) == 1:
         StopTime_two = time.time()
@@ -91,13 +102,16 @@ def distance_two():
     TimeElapsed_two = StopTime_two - StartTime_two
     distance_two = (TimeElapsed_two * 34300) / 2
  
-    q2.popleft()
-    q2.append(distance_two)
-    c = q2
-    c = sorted(c)
-    size = len(c)
-    median = c[(size-1)//2]
-    return median
+    if distance_two>0:
+        q2.popleft()
+        q2.append(distance_two)
+        c = q2
+        c = sorted(c)
+        size = len(c)
+        median = c[(size-1)//2]
+        return median
+    else:
+        return 404
 
 def distance_three():
 
@@ -108,8 +122,12 @@ def distance_three():
     StartTime_three = time.time()
     StopTime_three = time.time()
     t=0
+    tt=0
     while GPIO.input(echo_three) == 0:
         StartTime_three = time.time()
+        tt = StartTime_three - StopTime_three
+        if tt > 0.0058309038:      
+            break
  
     while GPIO.input(echo_three) == 1:
         StopTime_three = time.time()
@@ -120,13 +138,16 @@ def distance_three():
     TimeElapsed_three = StopTime_three - StartTime_three
     distance_three = (TimeElapsed_three * 34300) / 2
  
-    q3.popleft()
-    q3.append(distance_three)
-    c = q3
-    c = sorted(c)
-    size = len(c)
-    median = c[(size-1)//2]
-    return median
+    if distance_three>0:
+        q3.popleft()
+        q3.append(distance_three)
+        c = q3
+        c = sorted(c)
+        size = len(c)
+        median = c[(size-1)//2]
+        return median
+    else:
+        return 404
 
 def distance_four():
 
@@ -137,8 +158,12 @@ def distance_four():
     StartTime_four = time.time()
     StopTime_four = time.time()
     t=0
+    tt=0
     while GPIO.input(echo_four) == 0:
         StartTime_four = time.time()
+        tt = StartTime_four - StopTime_four
+        if tt > 0.0058309038:      
+            break
  
     while GPIO.input(echo_four) == 1:
         StopTime_four = time.time()
@@ -149,13 +174,16 @@ def distance_four():
     TimeElapsed_four = StopTime_four - StartTime_four
     distance_four = (TimeElapsed_four * 34300) / 2
  
-    q4.popleft()
-    q4.append(distance_four)
-    c = q4
-    c = sorted(c)
-    size = len(c)
-    median = c[(size-1)//2]
-    return median
+    if distance_four>0:
+        q4.popleft()
+        q4.append(distance_four)
+        c = q4
+        c = sorted(c)
+        size = len(c)
+        median = c[(size-1)//2]
+        return median
+    else:
+        return 404
 
 def distance_five():
 
@@ -166,8 +194,12 @@ def distance_five():
     StartTime_five = time.time()
     StopTime_five = time.time()
     t=0
+    tt=0
     while GPIO.input(echo_five) == 0:
         StartTime_five = time.time()
+        tt = StartTime_five - StopTime_five
+        if tt > 0.0058309038:      
+            break
  
     while GPIO.input(echo_five) == 1:
         StopTime_five = time.time()
@@ -177,16 +209,19 @@ def distance_five():
 
     TimeElapsed_five = StopTime_five - StartTime_five
     distance_five = (TimeElapsed_five * 34300) / 2
+    #distance_five = 100
  
-    q5.popleft()
-    q5.append(distance_five)
-    c = q5
-    c = sorted(c)
-    size = len(c)
-    median = c[(size-1)//2]
-    return median
+    if distance_five>0:
+        q5.popleft()
+        q5.append(distance)
+        c = q5
+        c = sorted(c)
+        size = len(c)
+        median = c[(size-1)//2]
+        return median
+    else:
+        return 404
 
- 
 def ultra_sensors():
     pub = rospy.Publisher('ultra_sensors',Float32MultiArray, queue_size=1)
     rospy.init_node('ultra_sensors', anonymous=True)
@@ -194,21 +229,31 @@ def ultra_sensors():
     #array = [23.0, 22.3, 3.2, 55.6, 12.4]
 
     while not rospy.is_shutdown():
+        #print("in while")
         d_one = distance_one()
+        #print("after distance 1")
         d_two = distance_two()
+        #print("after distance 2")
         d_three = distance_three()
+        #print("after distance 3")
         d_four = distance_four()
+        #print("after distance 4")
         d_five = distance_five()
+        #print("after distance 5")
         array = [d_one, d_two, d_three, d_four, d_five]
         ultra_str = Float32MultiArray(data = array)
+        #print("after ultra")
         rospy.loginfo(ultra_str)
         pub.publish(ultra_str)
+        #print("after publish")
         rate.sleep()
     if rospy.is_shutdown() == 1:
+        #print("rospy is shutdown")
         GPIO.cleanup()
  
 if __name__ == '__main__':
     try:
+        print("init")
         ultra_sensors()
 
     except rospy.ROSInterruptException:
